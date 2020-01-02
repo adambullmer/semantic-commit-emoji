8ff36a9bcc7eb3410d5715f32e1285df1e75ffc1
Adam Bullmer <psycodrumfreak@gmail.com>
2020-01-02 14:54:39 -0600

    :wrench:chore: Configure eslint to use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   rules: {
     "no-console": "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
