8aff59cd184a0756aa800279f32ab26e85be8192
Adam Bullmer <psycodrumfreak@gmail.com>
2020-01-02 16:51:04 -0600

    :sparkles:feat: Add merge, revert, and version hooks
    Resolves #6 #7 #8

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import * as fs from "fs";
 
+// Special automatic commit overrides
+const versionEmoji = "bookmark";
+const revertEmoji = "rewind";
+const mergeEmoji = "twisted_rightwards_arrows";
+
+// Semantic commit to emoji map
 const typeEmojiMap: { [key: string]: string | undefined } = {
   feat: "sparkles",
   fix: "bug",
@@ -15,6 +21,12 @@ const typeEmojiMap: { [key: string]: string | undefined } = {
   types: "label",
 };
 const allowedTypes = Object.keys(typeEmojiMap);
+const versionRegex = new RegExp(
+  /^v?([0-9]+).([0-9]+).([0-9]+)(?:-([0-9a-z-]+(?:.[0-9a-z-]+)*))?(?:\+[0-9a-z-]+)?$/,
+  "gi",
+);
+const revertRegex = new RegExp(/^revert(: | ")/, "gi");
+const mergeRegex = new RegExp(/^merge /, "gi");
 const semanticEmojiRegex = new RegExp(`^(:[a-z]{3,}:)?(${allowedTypes.join("|")})!?:`, "i");
 
 export default ([, , filePath]: typeof process.argv): void => {
@@ -24,6 +36,24 @@ export default ([, , filePath]: typeof process.argv): void => {
   function updateMessage(emoji: string): void {
     const newMessage = `:${emoji}:${commitString}`;
     fs.writeFileSync(filePath, newMessage);
+  }
+
+  const versionMatch = firstLine.match(versionRegex);
+  if (versionMatch !== null) {
+    updateMessage(versionEmoji);
+    return;
+  }
+
+  const mergeMatach = firstLine.match(mergeRegex);
+  if (mergeMatach !== null) {
+    updateMessage(mergeEmoji);
+    return;
+  }
+
+  const revertMatach = firstLine.match(revertRegex);
+  if (revertMatach !== null) {
+    updateMessage(revertEmoji);
+    return;
   }
 
   const match = firstLine.match(semanticEmojiRegex);
