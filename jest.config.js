9f10082d2b4d87a79f2c924fc4650abb384d92b4
Adam Bullmer <psycodrumfreak@gmail.com>
2020-01-03 02:17:54 -0600

    :wrench:chore: Configure initial jest config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  coverageDirectory: "artifacts/coverage",
+  testPathIgnorePatterns: ["/node-modules/", "/dist/"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "artifacts",
+        outputName: "test-results.xml",
+      },
+    ],
+  ],
+};
