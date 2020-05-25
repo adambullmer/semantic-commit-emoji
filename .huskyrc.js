fd4cd04b2f242ed136cc552f0e67175642316187
Adam Bullmer <psycodrumfreak@gmail.com>
2020-05-25 17:52:49 -0500

    :construction_worker:ci: Remove build from precommit hooks
    Let hooks fail if a build doesn't yet exist

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    "pre-commit": "yarn lint:ci && yarn build",
+    "pre-commit": "yarn lint:ci",
     "commit-msg": "commitlint -E HUSKY_GIT_PARAMS && node ./packages/cli/dist/cli.js $HUSKY_GIT_PARAMS",
   },
 };
