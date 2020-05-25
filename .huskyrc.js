5a9cba6e2da8abe37c7bf187af19fb574185d01c
Adam Bullmer <psycodrumfreak@gmail.com>
2020-05-25 16:50:53 -0500

    :construction_worker:ci: Update precommit hooks to build files
    Transpile source code since build tools depend on them

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    "pre-commit": "yarn lint:ci",
+    "pre-commit": "yarn lint:ci && yarn build",
     "commit-msg": "commitlint -E HUSKY_GIT_PARAMS && node ./packages/cli/dist/cli.js $HUSKY_GIT_PARAMS",
   },
 };
