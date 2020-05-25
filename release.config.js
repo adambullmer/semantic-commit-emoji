e2ed43a5d41c9756b4635294eae145447946af98
Adam Bullmer <psycodrumfreak@gmail.com>
2020-05-25 12:03:13 -0500

    :recycle:refactor: Rename import variable for better readability

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const parserOpts = require("commitlint-plugin-semantic-commit-emoji/dist/parser");
+const parserPreset = require("commitlint-plugin-semantic-commit-emoji/dist/parser");
 
 module.exports = {
   extends: "semantic-release-monorepo",
@@ -8,7 +8,7 @@ module.exports = {
       "@semantic-release/commit-analyzer",
       {
         preset: "conventionalcommits",
-        parserOpts: parserOpts.default.parserOpts,
+        parserOpts: parserPreset.default.parserOpts,
       },
     ],
     "@semantic-release/release-notes-generator",
