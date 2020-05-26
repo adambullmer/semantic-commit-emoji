41c8e92056fe15854d4ac629d62eb060e0f4f831
Adam Bullmer <psycodrumfreak@gmail.com>
2020-05-26 10:03:03 -0500

    :sparkles:feat(commitlint-plugin): Properly parse emoji in reverts and merges

--- packages/commitlint-plugin/parser.ts
+++ packages/commitlint-plugin/parser.ts
@@ -3,4 +3,11 @@ import { ParserOptions } from "@commitlint/types";
 export const parserOpts: ParserOptions = {
   headerPattern: /^(?::([^:])+:\s*)?(\w*)(?:\((.*)\))?!?: (.*)$/,
   headerCorrespondence: ["emoji", "type", "scope", "subject"],
+  commentChar: "#",
+  issuePrefixes: [],
+  mergePattern: /^(?::([^:])+:\s*)?Merge(?: (?:pull request #([^\d]+))? (?:from|branch) '?([^\s']+)'?(?: (?:in|into) ('?[^\s']+'?))?(?:\r?\n))*$/,
+  mergeCorrespondence: ["emoji", "pr", "head", "upstream"],
+  noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+  revertPattern: /^(?::([^:])+:\s*)?[Rr]evert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+  revertCorrespondence: ["emoji", "header", "hash"],
 };
