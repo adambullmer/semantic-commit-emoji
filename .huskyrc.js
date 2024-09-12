8d30926c9330a3a83ee57131f5a56f7d4132e4b1
Adam Bullmer <psycodrumfreak@gmail.com>
2024-09-12 10:38:38 -0500

    :wrench:chore: Update husky to latest 9.1.6

--- .huskyrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  hooks: {
-    "pre-commit": "lint-staged",
-    "commit-msg": "commitlint -E HUSKY_GIT_PARAMS && yarn node ./packages/cli/dist/cli.js $HUSKY_GIT_PARAMS",
-  },
-};
